2d9ae85f4baeea498ceee36c6f2e3048d92a26ea
arranzeyuwang <zywangx@gmail.com>
2023-04-01 23:28:37 -0400

    Initial Commit

---
+++ ColorMeasure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ColorMeasure
+//
+//  Created by Arran Wang on 4/1/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
